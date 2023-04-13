73f42c25d33d39633c47bc3becd6bb1fcb1befd4
Markuss-Matvejevs <124632442+Markuss-Matvejevs@users.noreply.github.com>
2023-04-13 22:08:55 +0300

    Add files via upload

--- hash_substring.py
+++ hash_substring.py
@@ -23,21 +23,39 @@ def hash(string, rolling_total = None):
 
     return hash(string[1:], ((rolling_total * 256) % 101 + ord(string[0])) % 101)
 
-def rehash(old_letter, old_hash, new_letter, hash_len):
-    return [ ( 4   + 101         -  97 * [(256%101)*256] % 101 ) * 256         +    97 ] % 101
+# computing the hash of the next substring:
+# https://en.wikipedia.org/wiki/Rabin%E2%80%93Karp_algorithm#Hash_function_used
+def rehash(old_letter, old_hash, new_letter, pattern_len):
+    base_value = 256
+
+    # raise 256 to the power of pattern_len - 1 and modulo 101 each value to avoid overflow
+    for i in range(pattern_len - 2):
+      base_value = (base_value % 101) * 256
+    
+    base_value %= 101
+    
+    return ((old_hash + 101 - ord(old_letter) * base_value) * 256 + ord(new_letter)) % 101
 
 def get_occurrences(pattern, text):
     pattern_hash = hash(pattern)
     slice_hash = None
-    occurrences = []
+    text_slice = ""
     pattern_len = len(pattern)
-    print(pattern_hash)
-    for i in range(len(text) - pattern_len + 1):
-      text_slice = text[i:i + pattern_len]
 
-      # calculate the new hash by adding only the character that was added to the slice
-      slice_hash = rehash(text_slice)
-      print(i, slice_hash)
+    occurrences = []
+    
+    for i in range(len(text) - pattern_len + 1):
+      new_slice = text[i:i + pattern_len]
+
+      if not slice_hash:
+        # we need to generate a hash for the slice when we are at the beginning of the input text
+        slice_hash = hash(new_slice)
+      else:
+        # calculate the new hash by adding only the character that was added to the slice
+        slice_hash = rehash(text_slice[0], slice_hash, new_slice[-1], pattern_len)
+        
+      text_slice = new_slice
+
       # only compare strings if the hashes match
       if pattern_hash == slice_hash and text_slice == pattern:
         occurrences.append(i)
